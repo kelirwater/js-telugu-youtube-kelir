b973ee2d3935367e643d3b407137b7751a96e58f
naveen <kelirwater99@gmail.com>
2025-03-28 21:01:41 +0530

    getter, setter with class, objects,function

---
+++ 10_classes _and_oop/object_base_get_set_.js
@@ -0,0 +1,15 @@
+const User = {
+  email: "h@hich.com",
+  password: "abc",
+
+  get email() {
+    return this._email ? this._email.toUpperCase() : "No email set";
+  },
+
+  set email(value) {
+    this._email = value;
+  },
+};
+
+const tea = Object.create(User);
+console.log(tea.email);
